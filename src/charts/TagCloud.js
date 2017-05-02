bf9358d7021b5fde8d6fc09c4eca90b773c3722c
blackart <blackart.ru@gmail.com>
2017-05-02 19:10:31 +0800

    w

--- src/charts/TagCloud.js
+++ src/charts/TagCloud.js
@@ -22,8 +22,17 @@ anychart.charts.TagCloud = function(opt_data, opt_settings) {
   this.ch = 1 << 11;
   this.size = [900, 600];
 
+  this.minFontSize = 10;
+  this.maxFontSize = 3000;
+
   this.colorScale = anychart.scales.linearColor(anychart.color.singleHueProgression('#3b5998'));
-  this.scale = anychart.scales.linear().minimum(10).maximum(150);
+  this.scale = anychart.scales.linear()
+      .minimum(this.minFontSize)
+      .maximum(this.maxFontSize);
+
+  this.angleCount = 7;
+  this.angleMin = -90;
+  this.angleMax = 90;
 
   this.data(opt_data || null, opt_settings);
 };
@@ -142,6 +151,60 @@ anychart.charts.TagCloud.prototype.angles = function() {
 
 //endregion
 //region --- Utils
+anychart.charts.TagCloud.prototype.getAngle = function(ratio) {
+  var range = (this.angleMax - this.angleMin);
+  return this.angleMin + (range / (this.angleCount - 1) * ratio);
+};
+
+/**
+ *  
+ */
+anychart.charts.TagCloud.prototype.calculateMaxFontSize = function(d) {
+  var scale = this.scale;
+  var w = this.w / 3;
+  var h = this.h / 3;
+
+  var evaluator = function(fontSize) {
+    ///
+    var bounds = acgraph.getRenderer().measure(d.text, {
+      'fontStyle': d.style,
+      'fontFamily': d.font,
+      'fontSize': fontSize,
+      'fontWeight': d.weight
+    });
+
+    var point = anychart.utils.getCoordinateByAnchor(bounds, /** @type {anychart.enums.Anchor} */('center'));
+    var tx = goog.math.AffineTransform.getRotateInstance(goog.math.toRadians(d.rotate), point.x, point.y);
+
+    var arr = bounds.toCoordinateBox() || [];
+    tx.transform(arr, 0, arr, 0, 4);
+
+    bounds = anychart.math.Rect.fromCoordinateBox(arr);
+    ///
+
+    var width = bounds.width;
+    var height = bounds.height;
+    var res;
+    if (width > w || height > h) {
+      res = -1;
+    } else if (width == w || height == h) {
+      res = 0;
+    } else {
+      res = 1;
+    }
+    return res;
+  };
+
+  var fonts = goog.array.range(this.minFontSize, this.maxFontSize + 1);
+  var res = goog.array.binarySelect(fonts, evaluator);
+  if (res < 0) {
+    res = ~res - 1;
+  }
+  var fontSize = fonts[goog.math.clamp(res, 0, fonts.length)];
+  scale.maximum(fontSize);
+};
+
+
 anychart.charts.TagCloud.prototype.cloudSprite = function(contextAndRatio, d, data, di) {
   if (d.sprite) return;
   var c = contextAndRatio.context,
@@ -156,8 +219,8 @@ anychart.charts.TagCloud.prototype.cloudSprite = function(contextAndRatio, d, da
   while (++di < n) {
     d = data[di];
     c.save();
-    c.font = d.style + " " + d.weight + " " + ~~ ((d.size + 1) / ratio) + "px " + d.font;
-    var w = c.measureText(d.text + "m").width * ratio,
+    c.font = d.style + ' ' + d.weight + ' ' + ~~ ((d.size + 1) / ratio) + 'px ' + d.font;
+    var w = c.measureText(d.text).width * ratio,
         h = d.size << 1;
     if (d.rotate) {
       var sr = Math.sin(d.rotate * this.cloudRadians),
@@ -303,7 +366,7 @@ anychart.charts.TagCloud.prototype.zeroArray = function(n) {
 };
 
 anychart.charts.TagCloud.prototype.cloudCanvas = function() {
-  return document.createElement("canvas");
+  return this.canvas ? this.canvas : (this.canvas = document.createElement("canvas"));
 };
 
 anychart.charts.TagCloud.prototype.getContext = function(canvas) {
@@ -323,17 +386,13 @@ anychart.charts.TagCloud.prototype.getContext = function(canvas) {
 };
 
 anychart.charts.TagCloud.prototype.place = function(board, tag, bounds) {
-  var perimeter = [{
-        x: 0,
-        y: 0
-      }, {
-        x: this.w,
-        y: this.h
-      }],
-      startX = tag.x,
+  var startX = tag.x,
       startY = tag.y,
       maxDelta = Math.sqrt(this.w * this.w + this.h * this.h),
+
       s = this.archimedeanSpiral(this.size),
+      // s = this.rectangularSpiral(this.size),
+
       // dt = Math.random() < .5 ? 1 : -1,
       dt = 1,
       t = -dt,
@@ -384,6 +443,10 @@ anychart.charts.TagCloud.prototype.place = function(board, tag, bounds) {
  * Calculating.
  */
 anychart.charts.TagCloud.prototype.calculate = function() {
+  var scale = this.scale;
+  var w = this.w;
+  var h = this.h;
+
   if (this.hasInvalidationState(anychart.ConsistencyState.TAG_CLOUD_DATA)) {
     var iterator = this.data_.getIterator();
 
@@ -398,79 +461,65 @@ anychart.charts.TagCloud.prototype.calculate = function() {
       });
     }
     var max = normalizedData[0].value;
-    var scale = this.scale;
+
 
     this.dataFormatted_ = normalizedData.map(function(d, i) {
       d.text = d.key;
       d.font = 'Times new roman';
       d.style = 'normal';
       d.weight = 'normal';
-      d.rotate = (~~(Math.random() * 6) - 3) * 30;
+      d.rotate = this.getAngle(~~(d.value % this.angleCount));
+      // d.rotate = (~~(Math.random() * 6) - 3) * 30;
       // d.rotate = 45;
       d.size = ~~scale.inverseTransform(d.value / max);
       d.sizeRatio = d.value / max;
       d.padding = 1;
       return d;
-    }).sort(function(a, b) {
+    }, this).sort(function(a, b) {
       return b.size - a.size;
     });
 
     this.markConsistent(anychart.ConsistencyState.TAG_CLOUD_DATA);
   }
 
-  // this.dataFormatted_.forEach(function(t) {
-  //   delete t.x;
-  //   delete t.x0;
-  //   delete t.x1;
-  //   delete t.xoff;
-  //   delete t.y;
-  //   delete t.y0;
-  //   delete t.y1;
-  //   delete t.yoff;
-  //   delete t.hasText;
-  //   delete t.height;
-  //   delete t.width;
-  // });
+  var n = this.dataFormatted_.length,
+      topTag = this.dataFormatted_[0],
+      i = -1,
+      bounds = null,
+      contextAndRatio = this.getContext(this.cloudCanvas()),
+      board = this.zeroArray((this.w >> 5) * this.h);
 
-  var n = this.dataFormatted_.length;
-  var i = -1;
-  var bounds = null;
-  var contextAndRatio = this.getContext(this.cloudCanvas());
-  var board = this.zeroArray((this.w >> 5) * this.h);
+  this.calculateMaxFontSize(topTag);
+  max = topTag.value;
+
+  //sets fontSize to tags.
+  this.dataFormatted_.forEach(function(d) {
+    d.size = ~~scale.inverseTransform(d.value / max);
+    delete d.sprite;
+  });
 
   while (++i < n) {
     var d = this.dataFormatted_[i];
-    // d.x = (this.w * (Math.random() + .5)) >> 1;
-    // d.y = (this.h * (Math.random() + .5)) >> 1;
     d.x = this.w >> 1;
     d.y = this.h >> 1;
     this.cloudSprite(contextAndRatio, d, this.dataFormatted_, i);
     if (d.hasText && this.place(board, d, bounds)) {
-      // tags.push(d);
-      // event.call("word", cloud, d);
       if (bounds) this.cloudBounds(bounds, d);
-      else bounds = [{
-        x: d.x + d.x0,
-        y: d.y + d.y0
-      }, {
-        x: d.x + d.x1,
-        y: d.y + d.y1
-      }];
-      // Temporary hack
+      else bounds = [{x: d.x + d.x0, y: d.y + d.y0}, {x: d.x + d.x1, y: d.y + d.y1}];
       d.x -= this.w >> 1;
       d.y -= this.h >> 1;
     }
   }
 
-  var w = this.w;
-  var h = this.h;
-
   var scale_ = bounds ? Math.min(w / Math.abs(bounds[1].x - w / 2), w / Math.abs(bounds[0].x - w / 2), h / Math.abs(bounds[1].y - h / 2), h / Math.abs(bounds[0].y - h / 2)) / 2 : 1;
+  if (!this.layer_) {
+    this.layer_ = this.container().unmanagedLayer();
+    this.textElementsLayer = acgraph.getRenderer().createLayerElement();
+    this.layer_.content(this.textElementsLayer);
+  }
 
-  var container = this.container().layer();
-  container
+  this.layer_
       .setTransformationMatrix(scale_, 0, 0, scale_, this.internalBounds_.left + (w >> 1), this.internalBounds_.top + (h >> 1));
-
 
   this.dataFormatted_.forEach(function(t) {
     var text;
@@ -478,34 +527,60 @@ anychart.charts.TagCloud.prototype.calculate = function() {
     // var text = t.textEl = container.text();
 
     if (t.textEl) {
-      t.textEl.parent(null);
-      t.textEl.parent(container);
+      // var dx = this.internalBounds_.left + this.internalBounds_.width / 2 - (t.srcParentBounds.left + t.srcParentBounds.width / 2);
+      // var dy = this.internalBounds_.top + this.internalBounds_.height / 2 - (t.srcParentBounds.top + t.srcParentBounds.height / 2);
 
-      t.textEl.domElement().setAttribute('transform', 'translate(' + [t.x, t.y] + ') rotate(' + t.rotate + ')');
+      // t.textEl.domElement().setAttribute('transform', 'translate(' + [t.x + dx, t.y + dy] + ') rotate(' + t.rotate + ')');
+      t.textEl.setAttribute('font-size', t.size);
+      t.textEl.setAttribute('transform', 'translate(' + [t.x, t.y] + ') rotate(' + t.rotate + ')');
+
+      // t.textEl.setTransformationMatrix(1, 0, 0, 1, 0, 0);
+      //
+      // t.textEl
+      //     .translate(t.x + dx, t.y + dy);
+      //
+      // var bounds = t.textEl.getBoundsWithTransform(t.textEl.getFullTransformation());
+      // var cx = bounds.left + bounds.width / 2;
+      // var cy = bounds.top + bounds.height / 2;
+      //
+      // t.textEl
+      //     .rotate(t.rotate, cx, cy);
 
       // t.textEl.setTransformationMatrix(1, 0, 0, 1, 0, 0);
       // t.textEl
       //     .translate(t.x, t.y)
       //     .rotateByAnchor(t.rotate);
     } else {
-      text = t.textEl = container.text();
+      // text = t.textEl = container.text();
+      text = t.textEl = acgraph.getRenderer().createTextElement();
+
+      // t.srcParentBounds = this.internalBounds_.clone();
 
       var fill = this.colorScale.valueToColor(t.sizeRatio);
-      text
-          .translate(t.x, t.y)
-          .rotateByAnchor(t.rotate)
-          .fontSize(t.size)
-          .fontFamily(t.font)
-          .text(t.text.toLowerCase())
-          .color(fill);
+      // text
+      //     .translate(t.x, t.y)
+      //     .rotateByAnchor(t.rotate)
+      //     .fontSize(t.size)
+      //     .fontFamily(t.font)
+      //     .text(t.text.toLowerCase())
+      //     .color(fill);
+
+      text.innerHTML = t.text.toLowerCase();
+      text.setAttribute('font-size', t.size);
+      text.setAttribute('font-family', t.font);
+      text.setAttribute('fill', fill);
+      text.setAttribute('transform', 'translate(' + [t.x, t.y] + ')rotate(' + t.rotate + ')');
+      text.setAttribute('text-anchor', 'middle');
+
+      this.textElementsLayer.appendChild(text);
 
       // text.domElement().setAttribute('transform', "translate(" + [t.x, t.y] + ")rotate(" + t.rotate + ")");
       // text.domElement().setAttribute('y', 0);
       // text.domElement().setAttribute('text-anchor', 'middle');
 
       // text.attr('transform', 'translate(' + [t.x, t.y] + ') rotate(' + t.rotate + ')');
-      text.attr('y', 0);
-      text.attr('text-anchor', 'middle');
+      // text.attr('y', 0);
+      // text.attr('text-anchor', 'middle');
     }
   }, this);
 };
@@ -513,9 +588,7 @@ anychart.charts.TagCloud.prototype.calculate = function() {
 
 //endregion
 //region --- Drawing
-/**
- * Drawing.
- */
+/** @inheritDoc */
 anychart.charts.TagCloud.prototype.drawContent = function(bounds) {
   this.internalBounds_ = bounds;
   this.w = bounds.width;
